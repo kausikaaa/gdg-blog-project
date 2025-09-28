a54373632c60664e8fadc1bbc191d8454160582c
kausikaaa <kauss0806@gmail.com>
2025-09-28 22:07:27 +0530

    feat(route): Secure PUT/DELETE post routes with protect middleware

--- server/routes/postRoutes.js
+++ server/routes/postRoutes.js
@@ -1,5 +1,6 @@
 const express = require('express');
-const { createPost, getPosts, getPostById } = require('../controllers/postController');
+const { createPost, getPosts, getPostById, updatePost, deletePost } = require('../controllers/postController');
+const { protect } = require('../middleware/authMiddleware');
 
 const router = express.Router();
 
@@ -11,5 +12,11 @@ router.get('/:id', getPostById);
 
 // POST /api/posts - Create a new post
 router.post('/', createPost);
+
+// PUT /api/posts/:id - Update a post (Protected)
+// DELETE /api/posts/:id - Delete a post (Protected)
+router.route('/:id')
+  .put(protect, updatePost)
+  .delete(protect, deletePost);
 
 module.exports = router;
