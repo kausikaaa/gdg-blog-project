06b1ba1e07be43b6bfe2d7ff90c5543f2b4af1c0
kausikaaa <kauss0806@gmail.com>
2025-09-28 21:30:17 +0530

    feat(route): Add GET /api/posts and GET /api/posts/:id routes

--- server/routes/postRoutes.js
+++ server/routes/postRoutes.js
@@ -1,7 +1,13 @@
 const express = require('express');
-const { createPost } = require('../controllers/postController');
+const { createPost, getPosts, getPostById } = require('../controllers/postController');
 
 const router = express.Router();
+
+// GET /api/posts - Get all posts
+router.get('/', getPosts);
+
+// GET /api/posts/:id - Get single post by ID
+router.get('/:id', getPostById);
 
 // POST /api/posts - Create a new post
 router.post('/', createPost);
